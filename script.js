29438a236809b7649ecaf2dd6dd350114af1ae96
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2022-11-10 12:24:53 +0000

    Responsivity done

--- script.js
+++ script.js
@@ -23,3 +23,16 @@ function scrollToContacts() {
     element.scrollIntoView({ behavior: 'smooth'});
 }   
 
+function submitForm() {
+    formData = new FormData(contactmeform);
+
+    formData.append('firstname', document.getElementById("input-first-name").value)
+    formData.append('secondname', document.getElementById("input-second-name").value)
+    formData.append('email', document.getElementById("input-email").value)
+    formData.append('message', document.getElementById("input-message").value)
+
+    console.log(formData.get('firstname'))
+    console.log(formData.get('secondname'))
+    console.log(formData.get('email'))
+    console.log(formData.get('message'))
+}
